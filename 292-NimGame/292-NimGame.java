e7f0d68074e7a785e7f5872ca40cd26822f8389a
Nagu Ganesh <vrnaguganesh@gmail.com>
2026-08-12 21:32:01 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 8/12/2026, 9:32:00 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0)
+        return true;
+        else
+       return false;
+
+    }
+}
