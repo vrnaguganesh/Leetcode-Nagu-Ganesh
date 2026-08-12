5750f60d6df61f26f909875d2c6665f2fe167ce8
Nagu Ganesh <vrnaguganesh@gmail.com>
2026-08-12 08:50:50 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,17 @@
+// Last updated: 8/12/2026, 8:50:49 AM
+class Solution {
+    public int numberOfSteps(int num) {
+        int c=0;
+        while(num!=0){
+        if(num%2==0){
+        num=num/2;
+        c++;
+        }
+        else{
+        num=num-1;
+        c++;
+        }
+        }
+        return c;
+        }
+}
